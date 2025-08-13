c16376a52f5304cf908b53887a2fabf8299eb13b
Hammad Ali <alih417399@gmail.com>
2025-08-13 15:13:22 +0500

    changed db url

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,15 +1,14 @@
-// mongodb+srv://<db_username>:<db_password>@cluster0.o2ecils.mongodb.net/?
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb+srv://admin:admin@cluster0.o2ecils.mongodb.net/food-del", {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
     console.log("DB Connected");
   } catch (error) {
     console.error("DB Connection Failed:", error.message);
-    process.exit(1); // Optional: Exit the process on connection failure
+    process.exit(1);
   }
 };
