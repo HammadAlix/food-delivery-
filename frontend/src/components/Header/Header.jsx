120b9155921f9f20e75eea1aff59098e2c97b77b
Hammad Ali <alih417399@gmail.com>
2025-08-17 14:25:03 +0500

    Fix Header background image using assets object

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,16 +1,20 @@
-import React from 'react'
-import "./Header.css"
+import React from 'react';
+import "./Header.css";
+import { assets } from '../../assets/assets'; // import the assets object
+
 const Header = () => {
   return (
-    <div className="header">
+    <div 
+      className="header" 
+      style={{ backgroundImage: `url(${assets.header_img})` }} // use assets.headerBg
+    >
       <div className="header-contents">
-        <h2>Order Your Favoutrite Food Here</h2>
+        <h2>Order Your Favourite Food Here</h2>
         <p>Indulge in delicious meals delivered right to your doorstep.</p>
         <button>View Menu</button>
       </div>
-      
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
