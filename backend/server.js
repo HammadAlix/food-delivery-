219d4f087ec0341a0e428786e3b149a06f234893
Hammad Ali <alih417399@gmail.com>
2025-08-17 14:30:17 +0500

    vercel confic changed

--- backend/server.js
+++ backend/server.js
@@ -1,33 +1,36 @@
-import express from "express";
-import cors from "cors";
-import serverless from "serverless-http";
+import express from "express"
+import cors from "cors"
 import { connectDB } from "./config/db.js";
 import foodRouter from "./routes/foodRoute.js";
 import UserRouter from "./routes/userRoute.js";
 import cartRouter from "./routes/cartRoute.js";
 import orderRouter from "./routes/orderRoute.js";
 
-const app = express();
+//app config
+const app=express();
+const port = process.env.PORT ||4000
 
-// Middleware
-app.use(express.json());
-app.use(cors());
 
-// Connect DB
+//middleware
+app.use(express.json()) 
+app.use(cors())
+
+//db
 connectDB();
 
-// Routes
-app.use("/api/food", foodRouter);
-app.use("/uploads", express.static("uploads"));
-app.use("/api/user", UserRouter);
+
+//api endpoints
+app.use("/api/food",foodRouter);
+app.use("/uploads", express.static("uploads"))
+app.use("/api/user",UserRouter);
 app.use("/api/cart", cartRouter);
 app.use("/api/order", orderRouter);
+ 
 
-// Test route
-app.get("/", (req, res) => {
-  res.send("API Working");
-});
+app.get("/",(req,res)=>{
+    res.send("API Working")
+})
 
-// Export for Vercel serverless
-export default serverless(app);
-
+app.listen(port,()=>{
+    console.log(`ServerStarted on http://localhost:${port}`)
+})
